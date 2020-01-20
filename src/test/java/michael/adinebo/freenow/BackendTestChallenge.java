392c02b9e9f3e40b5495c774db4f829869fe9466
Michael Adinebo <adinebomichael@gmail.com>
2020-01-20 04:36:00 +0100

    change source and target level to 1.8

--- src/test/java/michael/adinebo/freenow/BackendTestChallenge.java
+++ src/test/java/michael/adinebo/freenow/BackendTestChallenge.java
@@ -16,7 +16,7 @@ public class BackendTestChallenge {
     String userName = "Samantha";
 
     @Test
-    public void testTestAPILive() {
+    public void TestTestAPILive() {
         given().
                 when().
                 get("https://jsonplaceholder.typicode.com/users").
@@ -29,7 +29,7 @@ public class BackendTestChallenge {
 
     //Search for the user.
     @Test
-    public void testSearchUser() {
+    public void TestSearchUser() {
         HashMap<?, ?> userDetail = null;
         List<HashMap<?, ?>> users  = from(given().
                 when().
@@ -40,25 +40,25 @@ public class BackendTestChallenge {
                 userDetail = user;
             }
         }
-        testSearchUserPost(userDetail);
+        TestSearchUserPost(userDetail);
     }
 
     //Use the details fetched to make a search for the posts written by the user
-    public void testSearchUserPost(HashMap<?, ?> userDetail) {
+    public void TestSearchUserPost(HashMap<?, ?> userDetail) {
         List<HashMap<?, ?>> posts  = from(given().
                 when().
                 get("https://jsonplaceholder.typicode.com/posts").
                 asString()).getList("");
         for (HashMap<?, ?> post : posts) {
             if (post.get("userId").equals(userDetail.get("id"))) {
-                testValidateCommentsEmails(post);
+                TestValidateCommentsEmails(post);
             }
         }
     }
 
     //For each post, fetch the comments and validate if the emails in the comment
     //section are in the proper format
-    public void testValidateCommentsEmails(HashMap<?, ?> post) {
+    public void TestValidateCommentsEmails(HashMap<?, ?> post) {
         List<HashMap<String, String>> comments  = from(given().
                 when().
                 get("https://jsonplaceholder.typicode.com/comments").
